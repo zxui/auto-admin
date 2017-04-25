00a31d266935a8b19d651aa1607a6be2135e0409
zxshen2 <zxshen2@iflytek.com>
2017-04-25 12:17:06 +0800

    新增基础组件

---
+++ src/pages/Button.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <el-button type="primary" icon="edit"></el-button>
+        <el-button type="primary" icon="share"></el-button>
+        <el-button type="primary" icon="delete"></el-button>
+        <el-button type="primary" icon="search">搜索</el-button>
+        <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+    </div>
+</template>
