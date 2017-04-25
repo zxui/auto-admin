5a052cfe71e89317de015582600317fa683c93ac
zxui <zxshen@qq.com>
2017-04-25 19:09:14 +0800

    修改预览分支

--- src/pages/Progress.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <el-progress :percentage="0"></el-progress>
-        <el-progress :percentage="70"></el-progress>
-        <el-progress :percentage="100" status="success"></el-progress>
-        <el-progress :percentage="50" status="exception"></el-progress>
-    </div>
-</template>
